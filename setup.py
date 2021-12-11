3e1baeb88902beaedec25d4c69dff77a00d87bd9
MurdoMaclachlan <47920497+MurdoMaclachlan@users.noreply.github.com>
2021-12-11 00:36:13 +0000

    Add refresh token support, refactor Logger

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
-from core.classes import VERSION
+from tadrcore.globals import VERSION
 
 
 def readme():
-    return open('README.md', 'r').read()
+    return open("README.md", "r").read()
 
 
 setup(
@@ -12,20 +12,19 @@ setup(
     scripts=["tadr"],
     author="Murdo Maclachlan",
     author_email="murdo@maclachlans.org.uk",
-    description="A tool that will automatically reply done to the first 'cannot find transcription' message from the r/TranscribersOfReddit bot. ",
+    description=(
+        "A tool that will automatically reply done to the first 'cannot find"
+        " transcription' message from the r/TranscribersOfReddit bot."
+    ),
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/MurdoMaclachlan/tadr",
     packages=find_packages(),
-    install_requires=[
-        "colored",
-        "configparser",
-        "praw>=7.1.2"
-    ],
+    install_requires=["colored", "configparser", "praw>=7.5.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    license='GPLv3+'
+    license="GPLv3+",
 )
